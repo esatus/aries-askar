5044940ff9b749a29d119faf3f67e3a42edba7dc
Jan Wameling <j.wameling@esatus.com>
2022-06-22 17:06:42 +0200

    Added ScretApi and prepared test class

---
+++ wrappers/dotnet/aries-askar-dotnet/aries-askar/SecretApi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static indy_vdr_dotnet.models.Structures;
+
+namespace aries_askar_dotnet.aries_askar
+{
+    public class SecretApi
+    {
+        public static async Task BufferFree(SecretBuffer buffer)
+        {
+            int errorCode = NativeMethods.askar_buffer_free(buffer);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.askar_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
+    }
+}
