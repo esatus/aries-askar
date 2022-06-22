28edb2d0cff4e1d89c0f00efc435786d2dddfc6e
Jan Wameling <j.wameling@esatus.com>
2022-06-22 15:10:21 +0200

    added testclass for ResultListApi

---
+++ wrappers/dotnet/aries-askar-dotnet-tests/aries-askar/ResultListApiTests.cs
@@ -0,0 +1,17 @@
+﻿using aries_askar_dotnet.aries_askar;
+using aries_askar_dotnet.Models;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static indy_vdr_dotnet.models.Structures;
+
+namespace aries_askar_dotnet_tests.aries_askar
+{
+    public class ResultListApiTests
+    {
+    }
+}
