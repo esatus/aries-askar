35f77d3372b44e165681618fd8528c1feabe4c01
SMichel-esatus <s.michel@esatus.com>
2023-04-27 11:49:20 +0200

    add xamarin ios target

--- wrappers/dotnet/aries-askar-dotnet/Consts.cs
+++ wrappers/dotnet/aries-askar-dotnet/Consts.cs
@@ -2,6 +2,10 @@
 {
     public static class Consts
     {
+#if __IOS__
+        public const string ARIES_ASKAR_LIB_NAME = "__Internal";
+#else
         public const string ARIES_ASKAR_LIB_NAME = "aries_askar";
+#endif
     }
 }
